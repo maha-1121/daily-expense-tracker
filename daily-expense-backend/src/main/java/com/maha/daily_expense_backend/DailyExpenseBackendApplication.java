dfa4dc28fb31d13f8d4ea94906bc5229d7334ab6
maha-1121 <mahalakshmi2114@gmail.com>
2025-09-05 08:32:46 +0530

    chore(backend): add Spring Boot scaffold correctly

---
+++ daily-expense-backend/src/main/java/com/maha/daily_expense_backend/DailyExpenseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.maha.daily_expense_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyExpenseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyExpenseBackendApplication.class, args);
+	}
+
+}
---
+++ daily-expense-backend/src/test/java/com/maha/daily_expense_backend/DailyExpenseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maha.daily_expense_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyExpenseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
